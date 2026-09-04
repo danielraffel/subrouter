ce4cbf7912bc9168b1605d2b0eeb0aeab1c2238a
daniel raffel <daniel.raffel@gmail.com>
2026-09-04 13:51:07 -0700

    ui: omit unavailable OpenRouter auto-top-up state

--- cmd/subrouter/sr.go
+++ cmd/subrouter/sr.go
@@ -4026,8 +4026,6 @@ func usageGridCreditsCell(row srUsageRow) usageGridCell {
 					} else {
 						text += "; top-up off"
 					}
-				} else {
-					text += "; top-up ?"
 				}
 			}
 			return usageGridCell{Text: text}
